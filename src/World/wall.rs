43d03f14b25a28ead4bbcc311d1df479052fe60d
Takumi Crary <craryg@oregonstate.edu>
2018-04-11 00:48:11 -0700

    Add World and Wall modules

---
+++ src/World/wall.rs
@@ -0,0 +1,52 @@
+use sdl2::pixels::Color;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Wall {
+    pub full: bool,
+    pub color: Color,
+}
+
+pub const NULL_WALL: Wall = Wall {
+    full: false,
+    color: NULL_COLOR,
+};
+
+
+pub const NULL_COLOR: Color = Color {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 0,
+};
+
+pub const RED: Color = Color {
+    r: 255,
+    g: 0,
+    b: 0,
+    a: 255,
+};
+pub const GREEN: Color = Color {
+    r: 0,
+    g: 255,
+    b: 0,
+    a: 255,
+};
+pub const BLUE: Color = Color {
+    r: 0,
+    g: 0,
+    b: 255,
+    a: 255,
+};
+
+pub const FLOOR_GREY: Color = Color {
+    r: 128,
+    g: 128,
+    b: 128,
+    a: 255,
+};
+pub const CEILING_BLACK: Color = Color {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 255,
+};
