9efcd2cc8fa5aa756fad886aca3ea3f3820ab2fd
Takumi Crary <craryg@oregonstate.edu>
2018-05-30 23:05:27 -0700

    Add beginings of windows build script for wrangling SDL2 deps

---
+++ windows_build_script_ready.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -e SDL2-devel-2.0.8-mingw.tar.gz ] #&& [ -e SDL2-devel-2.0.8-VC.zip ]
+then
+    rm -rf SDL2-2.0.8/
+    rm -rf gnu-mingw
+    rm -rf msvc
+    tar xzf SDL2-devel-2.0.8-mingw.tar.gz
+    #unzip -f SDL2-devel-2.0.8-VC.zip
+
+    mkdir -p gnu-mingw/dll/32
+    mkdir -p gnu-mingw/dll/64
+    mkdir -p gnu-mingw/lib/32
+    mkdir -p gnu-mingw/lib/64
+    #mkdir -p msvc/dll/32
+    #mkdir -p msvc/dll/64
+    #mkdir -p msvc/lib/32
+    #mkdir -p msvc/lib/64
+fi
