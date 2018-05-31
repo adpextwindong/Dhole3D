54bf542a105681b76999e1e7744515494b208431
Takumi Crary <craryg@oregonstate.edu>
2018-05-31 01:33:39 -0700

    Fix window building

--- windows_build_script_ready.sh
+++ windows_build_script_ready.sh
@@ -9,9 +9,17 @@ then
     #unzip -f SDL2-devel-2.0.8-VC.zip
 
     mkdir -p gnu-mingw/dll/32
-    mkdir -p gnu-mingw/dll/64
     mkdir -p gnu-mingw/lib/32
+    
+    mkdir -p gnu-mingw/dll/64
     mkdir -p gnu-mingw/lib/64
+
+    cp SDL2-2.0.8/i686-w64-mingw32/bin/* gnu-mingw/dll/32/
+    cp SDL2-2.0.8/i686-w64-mingw32/lib/* gnu-mingw/lib/32/
+
+    cp SDL2-2.0.8/x86_64-w64-mingw32/bin/* gnu-mingw/dll/64/
+    cp SDL2-2.0.8/x86_64-w64-mingw32/lib/* gnu-mingw/lib/64/
+
     #mkdir -p msvc/dll/32
     #mkdir -p msvc/dll/64
     #mkdir -p msvc/lib/32
